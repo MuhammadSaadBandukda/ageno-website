406c864f5eab974ec7ca9c006a09b38e0f5d1089
Muhammad Saad <saadsaleem997@gmail.com>
2025-07-19 17:45:33 +0500

    footer-responsive

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,11 @@
 import { Link } from "react-router-dom";
 const Footer = () => {
   return (
-    <div className="bg-white dark:bg-zinc-800 dark:text-gray-400 space-y-12">
-    <div className="grid grid-cols-12  mx-auto px-4 sm:px-6 lg:px-8">
-      <div className="col-span-6 mx-auto md:mx-0">
+    <div className="w-full bg-white dark:bg-zinc-800 dark:text-gray-400 space-y-6 md:space-y-12">
+    <div className="grid md:grid-cols-12 mx-auto px-4 sm:px-6 lg:px-8 space-y-4">
+      <div className="md:col-span-6 place-items-center mx-auto md:mx-0">
         <h1 className="text-blue-600 text-xl font-bold">Agenoverse</h1>
-        <p className="w-3/4">
+        <p className="w-3/4 text-sm sm:text-lg text-center sm:text-justify md:text-left">
           Agenoverse is a futuristic community where learners, innovators, and
           builders come together to explore the world of AI agents, generative
           intelligence, and breakthrough ideas. It's a universe where we learn
@@ -13,9 +13,9 @@ const Footer = () => {
           member to become not just a user of AI, but a creator of it.
         </p>
       </div>
-      <div className="col-span-2 ">
+      <div className="md:col-span-2 place-items-center">
         <h2 className="dark:text-white font-medium ">Resources</h2>
-        <ul className="space-y-3">
+        <ul className="md:space-y-3 place-items-center">
           <li className="hover:underline dark:hover:text-white">
             <Link to="/services">Services</Link>
           </li>
@@ -33,9 +33,9 @@ const Footer = () => {
           </li>
         </ul>
       </div>
-      <div className="col-span-2">
+      <div className="md:col-span-2 place-items-center">
         <h2 className="dark:text-white font-medium ">Help & Support</h2>
-        <ul className="space-y-3">
+        <ul className="md:space-y-3 place-items-center ">
           <li className="hover:underline dark:hover:text-white">
             <Link to="/contact-us">Contact Us</Link>
           </li>
@@ -44,9 +44,9 @@ const Footer = () => {
           </li>
         </ul>
       </div>
-      <div className="col-span-2">
+      <div className="md:col-span-2 place-items-center">
         <h2 className="dark:text-white font-medium ">Follow Us</h2>
-        <ul className="space-y-3">
+        <ul className="md:space-y-3 place-items-center">
           <li className="hover:underline dark:hover:text-white">
             <Link to="https://www.facebook.com/profile.php?id=61574935032238">Facebook</Link>
           </li>
@@ -57,7 +57,7 @@ const Footer = () => {
             <Link to="https://github.com/agenoverse">Github</Link>
           </li>
           <li className="hover:underline dark:hover:text-white">
-            <Link to="https://x.com/agenoverse">X(formerly Twitter)</Link>
+            <Link to="https://x.com/agenoverse">X(Twitter)</Link>
           </li>
           <li className="hover:underline dark:hover:text-white">
             <Link to="https://www.instagram.com/agenoverse.ai/">Instagram</Link>
